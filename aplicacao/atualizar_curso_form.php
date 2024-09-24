aca83180074585ba2a48657c246cdba6f9b2f816
rolim265 <gabrielrodriguesrolim@gmail.com>
2024-09-24 08:59:13 -0300

    yy6ty

--- aplicacao/atualizar_curso_form.php
+++ aplicacao/atualizar_curso_form.php
@@ -1,119 +1,125 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-br"> <!-- Define o tipo de documento e o idioma da página como português do Brasil -->
 
 <head>
-    <title>Atualizar Curso</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Atualizar Curso</title> <!-- Define o título da página que aparece na aba do navegador -->
+    <meta charset="utf-8"> <!-- Define a codificação de caracteres como UTF-8 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui"> <!-- Configura a visualização para dispositivos móveis -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Define a compatibilidade do IE para a versão mais recente -->
 
-    <meta name="keywords" content="bootstrap, bootstrap admin template, admin theme, admin dashboard, dashboard template, admin template, responsive" />
-    <meta name="author" content="Codedthemes" />
+    <meta name="keywords" content="bootstrap, bootstrap admin template, admin theme, admin dashboard, dashboard template, admin template, responsive" /> <!-- Palavras-chave para SEO -->
+    <meta name="author" content="Codedthemes" /> <!-- Autor do documento -->
+    
     <!-- Favicon icon -->
-    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon"> <!-- Define o ícone da aba do navegador -->
+    
     <!-- Google font-->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet"> <!-- Importa a fonte "Open Sans" do Google Fonts -->
+    
     <!-- waves.css -->
-    <link rel="stylesheet" href="assets/pages/waves/css/waves.min.css" type="text/css" media="all">
-    <!-- Required Fremwork -->
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/pages/waves/css/waves.min.css" type="text/css" media="all"> <!-- Importa o CSS para efeitos de onda -->
+    
+    <!-- Required Framework -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap/css/bootstrap.min.css"> <!-- Importa o CSS do Bootstrap -->
+    
     <!-- waves.css -->
-    <link rel="stylesheet" href="assets/pages/waves/css/waves.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="assets/pages/waves/css/waves.min.css" type="text/css" media="all"> <!-- Importa novamente o CSS para efeitos de onda, redundante -->
+    
     <!-- themify icon -->
-    <link rel="stylesheet" type="text/css" href="assets/icon/themify-icons/themify-icons.css">
+    <link rel="stylesheet" type="text/css" href="assets/icon/themify-icons/themify-icons.css"> <!-- Importa o CSS para ícones Themify -->
+    
     <!-- font-awesome-n -->
-    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome-n.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome-n.min.css"> <!-- Importa o CSS para ícones Font Awesome (versão normal) -->
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css"> <!-- Importa o CSS para ícones Font Awesome (versão minificada) -->
+    
     <!-- scrollbar.css -->
-    <link rel="stylesheet" type="text/css" href="assets/css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/jquery.mCustomScrollbar.css"> <!-- Importa o CSS para barra de rolagem personalizada -->
+    
     <!-- Style.css -->
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"> <!-- Importa o CSS principal da aplicação -->
 </head>
 
 <body>
     
 <?php
-    include 'menu_principal.php';
+    include 'menu_principal.php'; // Inclui o arquivo 'menu_principal.php', que provavelmente contém o menu de navegação
 ?>
 
-                        <div class="pcoded-inner-content">
-                            <!-- Main-body start -->
-                            <div class="main-body">
-                                <div class="page-wrapper">
-                                    <!-- Page-body start -->
-                                    <div class="page-body">
-                                        <div class="row">
-                                            <!-- Project statustic start -->
-                                            <div class="col-xl-12">
-                                                <div class="row">
-                                                    <div class="col-xl-8 col-md-10 m-auto">
-                                                        <h2>Escolha um curso para ser atualizado</h2>
-                                                        <table class="table">
-                                                            <thead>
-                                                                <tr>
-                                                                <th scope="col">ID</th>
-                                                                <th scope="col">NOME DO CURSO</th>
-                                                                <th scope="col">OPERAÇÃO</th>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                            <?php
-                                                                $url = 'http://localhost/exercicio/api.php/cursos';
-                                                                $response = file_get_contents($url);
-                                                                $data = json_decode($response, true);
+    <div class="pcoded-inner-content"> <!-- Início do conteúdo interno do layout -->
+        <!-- Main-body start -->
+        <div class="main-body"> <!-- Div principal do corpo -->
+            <div class="page-wrapper"> <!-- Envolvimento para a página -->
+                <!-- Page-body start -->
+                <div class="page-body"> <!-- Início do corpo da página -->
+                    <div class="row"> <!-- Início da linha -->
+                        <!-- Project statistic start -->
+                        <div class="col-xl-12"> <!-- Coluna que ocupa toda a largura em telas grandes -->
+                            <div class="row"> <!-- Início de uma nova linha -->
+                                <div class="col-xl-8 col-md-10 m-auto"> <!-- Coluna centralizada que ocupa 8 de 12 colunas em telas grandes e 10 em médias -->
+                                    <h2>Escolha um curso para ser atualizado</h2> <!-- Título da seção -->
+                                    <table class="table"> <!-- Início da tabela -->
+                                        <thead> <!-- Cabeçalho da tabela -->
+                                            <tr> <!-- Linha do cabeçalho -->
+                                                <th scope="col">ID</th> <!-- Cabeçalho para a coluna de ID -->
+                                                <th scope="col">NOME DO CURSO</th> <!-- Cabeçalho para a coluna de nome do curso -->
+                                                <th scope="col">OPERAÇÃO</th> <!-- Cabeçalho para a coluna de operações -->
+                                            </tr>
+                                        </thead>
+                                        <tbody> <!-- Corpo da tabela -->
+                                        <?php
+                                            $url = 'http://localhost/exercicio/api.php/cursos'; // URL da API que retorna a lista de cursos
+                                            $response = file_get_contents($url); // Faz uma requisição GET para a API
+                                            $data = json_decode($response, true); // Decodifica a resposta JSON em um array associativo
 
-                                                                if (isset($data['dados'])) {                                                                    
-                                                                    foreach ($data['dados'] as $curso) {
-                                                                        echo "<tr>";
-                                                                        echo '<td>' . $curso['id_curso'] . '</td>';
-                                                                        echo '<td>' . $curso['nome_curso'] . '</td>';
-                                                                        echo '<td> 
-                                                                                <a href="atualiza_curso.php?id='.$curso['id_curso'].'" class="btn btn-primary">ATUALIZAR</a> 
-                                                                            </td>';
-                                                                        echo "</tr>";            
-                                                                    }
-                                                                } else {
-                                                                    echo '<p>Nenhum curso encontrado.</p>';
-                                                                }
-                                                            ?>                                                                    
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <!-- Project statustic end -->
-                                        </div>
-                                    </div>
-                                    <!-- Page-body end -->
+                                            if (isset($data['dados'])) { // Verifica se a chave 'dados' existe no array
+                                                foreach ($data['dados'] as $curso) { // Itera sobre cada curso encontrado
+                                                    echo "<tr>"; // Início de uma nova linha na tabela
+                                                    echo '<td>' . $curso['id_curso'] . '</td>'; // Exibe o ID do curso
+                                                    echo '<td>' . $curso['nome_curso'] . '</td>'; // Exibe o nome do curso
+                                                    echo '<td> 
+                                                            <a href="atualiza_curso.php?id='.$curso['id_curso'].'" class="btn btn-primary">ATUALIZAR</a> 
+                                                        </td>'; // Link para a página de atualização do curso com um botão estilizado
+                                                    echo "</tr>"; // Fim da linha
+                                                }
+                                            } else {
+                                                echo '<p>Nenhum curso encontrado.</p>'; // Mensagem caso não haja cursos disponíveis
+                                            }
+                                        ?>                                                                    
+                                        </tbody>
+                                    </table>
                                 </div>
-                                <div id="styleSelector"> </div>
                             </div>
                         </div>
+                        <!-- Project statistic end -->
                     </div>
                 </div>
+                <!-- Page-body end -->
             </div>
+            <div id="styleSelector"> </div> <!-- Seletor de estilo, pode ser utilizado para alterar temas -->
         </div>
     </div>
     
 
     <!-- Required Jquery -->
-    <script type="text/javascript" src="assets/js/jquery/jquery.min.js "></script>
-    <script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.min.js "></script>
-    <script type="text/javascript" src="assets/js/popper.js/popper.min.js"></script>
-    <script type="text/javascript" src="assets/js/bootstrap/js/bootstrap.min.js "></script>
+    <script type="text/javascript" src="assets/js/jquery/jquery.min.js "></script> <!-- Importa o jQuery -->
+    <script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.min.js "></script> <!-- Importa o jQuery UI -->
+    <script type="text/javascript" src="assets/js/popper.js/popper.min.js"></script> <!-- Importa a biblioteca Popper.js para gerenciamento de pop-ups e tooltips -->
+    <script type="text/javascript" src="assets/js/bootstrap/js/bootstrap.min.js "></script> <!-- Importa o Bootstrap JS -->
+    
     <!-- waves js -->
-    <script src="assets/pages/waves/js/waves.min.js"></script>
+    <script src="assets/pages/waves/js/waves.min.js"></script> <!-- Importa o JavaScript para efeitos de onda -->
+    
     <!-- jquery slimscroll js -->
-    <script type="text/javascript" src="assets/js/jquery-slimscroll/jquery.slimscroll.js"></script>
-
+    <script type="text/javascript" src="assets/js/jquery-slimscroll/jquery.slimscroll.js"></script> <!-- Importa o JavaScript para rolagem slim -->
+    
     <!-- slimscroll js -->
-    <script src="assets/js/jquery.mCustomScrollbar.concat.min.js "></script>
-
+    <script src="assets/js/jquery.mCustomScrollbar.concat.min.js "></script> <!-- Importa o JavaScript para barra de rolagem personalizada -->
+    
     <!-- menu js -->
-    <script src="assets/js/pcoded.min.js"></script>
-    <script src="assets/js/vertical/vertical-layout.min.js "></script>
-
-    <script type="text/javascript" src="assets/js/script.js "></script>
+    <script src="assets/js/pcoded.min.js"></script> <!-- Importa o JavaScript principal do layout -->
+    <script src="assets/js/vertical/vertical-layout.min.js "></script> <!-- Importa o layout vertical do JavaScript -->
+    
+    <script type="text/javascript" src="assets/js/script.js "></script> <!-- Importa o JavaScript personalizado -->
 </body>
 
 </html>
--- aplicacao/cadastra_aluno.php
+++ aplicacao/cadastra_aluno.php
@@ -1,30 +1,47 @@
 <?php
+    // Verifica se a requisição foi feita via método POST
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Captura o nome do aluno enviado pelo formulário
         $nome_aluno = $_POST['nome'];
+        // Captura o e-mail do aluno enviado pelo formulário
         $email_aluno = $_POST['email'];
+        // Captura o ID do curso associado ao aluno enviado pelo formulário
         $id_curso = $_POST['curso_id'];
 
+        // Cria um array com os dados do aluno a serem enviados
         $data = array("nome" => $nome_aluno, "email" => $email_aluno, "fk_cursos_id_curso" => $id_curso);
+        
+        // Define as opções para a requisição HTTP
         $options = array(
             'http' => array(
+                // Define o cabeçalho da requisição como JSON
                 'header'  => "Content-type: application/json\r\n",
+                // Define o método HTTP como POST
                 'method'  => 'POST',
+                // Codifica o array de dados em JSON e o define como conteúdo da requisição
                 'content' => json_encode($data),
             ),
         );
 
+        // Cria um contexto de stream com as opções definidas
         $context  = stream_context_create($options);
+        
+        // Define a URL da API onde os dados serão enviados
         $url = 'http://localhost/exercicio/api.php/alunos';
+        
+        // Faz a requisição HTTP para a API e armazena o resultado
         $result = file_get_contents($url, false, $context);
 
+        // Verifica se a requisição falhou
         if ($result === FALSE) {
+            // Exibe uma mensagem de erro se a adição do aluno falhar
             echo "<p>Erro ao adicionar aluno.</p>";
         } else { ?>
             <script>
-                // Exibe o alerta
+                // Exibe um alerta de sucesso
                 alert('Operação concluída com sucesso!');
 
-                // Após o usuário clicar em "OK", redireciona para outra página
+                // Após o usuário clicar em "OK", redireciona para a página 'lista_alunos.php'
                 window.location.href = 'lista_alunos.php'; // Altere para a página de destino
             </script>
         <?php    
